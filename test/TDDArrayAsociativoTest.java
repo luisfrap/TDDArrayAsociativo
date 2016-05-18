7b339c9560693432cb15e8c92f60e4245f256d69
luisfrap <luisfraipas@gmail.com>
2016-05-18 19:14:05 +0200

    Test Comenzado

--- test/TDDArrayAsociativoTest.java
+++ test/TDDArrayAsociativoTest.java
@@ -1,12 +1,24 @@
 import static org.junit.Assert.*;
 
+import java.util.LinkedList;
+
 import org.junit.Test;
 
 public class TDDArrayAsociativoTest {
+	private TDDArrayAsociativo arrayAsociativo = new TDDArrayAsociativo();
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void crearListaVacia() {
+		LinkedList lista = new LinkedList();
+		assertTrue(lista.isEmpty());
+	}
+
+	@Test
+	public void anadirElementoALaLista() {
+		LinkedList<Integer> lista = new LinkedList<Integer>();
+		lista.add(1);
+		assertFalse(lista.isEmpty());
+		assertTrue(lista.contains(1));
 	}
 
 }
