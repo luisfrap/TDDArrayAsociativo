e7a67f05e88b509d1f377cd1aeb75e7970c9e0d8
luisfrap <luisfraipas@gmail.com>
2016-05-19 18:34:44 +0200

    Creamos codigo de produccion y excepcion para superar el primer test

--- src/TDDArrayAsociativo.java
+++ src/TDDArrayAsociativo.java
@@ -1,11 +1,27 @@
+import java.util.Hashtable;
 import java.util.Scanner;
 
 public class TDDArrayAsociativo {
 
-	public Object get(String string) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
+	private Hashtable<String, String> tabla = new Hashtable<String, String>();
 
+	public void put(String clave, String valor) {
+		if (tabla.contains(clave)) {
+			tabla.replace(clave, valor);
+		} else {
+			tabla.put(clave, valor);
+		}
+
+	}
+
+	public String get(String clave) {
+		String get ="";
+		if(tabla.containsKey(clave)){
+			get = tabla.get(clave);
+		}else{
+			throw new TDDException(tabla.toString());
+		}
+		return get;
+		
+	}
 }
